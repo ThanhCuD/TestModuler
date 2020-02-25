1a866570a4a86d590ba75d3d5097ae47d741d21c
ThanhCuD <thanhduc552018@gmail.com>
2020-02-25 09:40:43 +0700

    change

--- XModuler/src/Module.Infrastructure/GlobalConfiguration.cs
+++ XModuler/src/Module.Infrastructure/GlobalConfiguration.cs
@@ -8,17 +8,8 @@ namespace Module.Infrastructure
     {
         public static IList<ModuleInfo> Modules { get; set; } = new List<ModuleInfo>();
 
-        public static string DefaultCulture => "en-US";
-
         public static string WebRootPath { get; set; }
 
         public static string ContentRootPath { get; set; }
-
-        public static IList<string> AngularModules { get; } = new List<string>();
-
-        public static void RegisterAngularModule(string angularModuleName)
-        {
-            AngularModules.Add(angularModuleName);
-        }
     }
 }
